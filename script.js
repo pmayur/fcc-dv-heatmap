1eaa97740ad9ccba0f27b3682a247b4c52582d89
pmayur <thisispatil@gmail.com>
2020-11-26 19:46:28 +0530

    Append svg element to the dom

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(d3)
+// Link for data about yearly global temperatures (1775 - 2015)
+const DATA_SOURCE =
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+
+// Dimensions for graph
+var margin = { top: 30, right: 30, bottom: 30, left: 30 },
+    width = 450 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3
+    .select("#root")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
