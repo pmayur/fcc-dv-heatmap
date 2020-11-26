77455d3716cf7a094c5ec3e935e5c0b87376a39b
pmayur <thisispatil@gmail.com>
2020-11-26 21:13:11 +0530

    Append Y Axis to svg element

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const MONTHS = [
 ];
 
 // Dimensions for graph
-var margin = { top: 30, right: 30, bottom: 30, left: 30 },
+var margin = { top: 30, right: 30, bottom: 30, left: 60 },
     width = 1200 - margin.left - margin.right,
     height = 450 - margin.top - margin.bottom;
 
@@ -41,19 +41,29 @@ d3.json(DATA_SOURCE).then((data) => {
         TO: data.monthlyVariance[data.monthlyVariance.length - 1].year,
     };
 
-    // X AXIS
+    /* =========================== X AXIS ============================ */
     var x = d3
         .scaleTime()
         .range([0, width])
-        .domain([
-            new Date(`${DATE.FROM}`), 
-            new Date(`${DATE.TO}`)
-        ]);
+        .domain([new Date(`${DATE.FROM}`), new Date(`${DATE.TO}`)]);
 
     // Append X AXIS to svg element
     svg.append("g")
         .style("font-size", 15)
         .attr("transform", "translate(0," + height + ")")
         .attr("id", "x-axis")
-        .call(d3.axisBottom(x).ticks((DATE.TO - DATE.FROM) / 10));
+        .call(
+            d3
+                .axisBottom(x)
+                .ticks((DATE.TO - DATE.FROM) / 10)
+                .tickSizeOuter(0)
+        );
+
+    /* =========================== Y AXIS ============================ */
+    var y = d3.scaleBand().range([height, 0]).domain(MONTHS.reverse());
+
+    // Append Y AXIS to the svg element
+    svg.append("g")
+        .attr("id", "y-axis")
+        .call(d3.axisLeft(y).tickSizeOuter(0));
 });
